17cf59c4a2ab77e1282f201103aba32d3ff3f372
Laura <hulubalaura31@gmail.com>
2022-04-27 15:32:07 +0300

    tema 01 - changed changed method name

--- src/ro/ase/cts/gr1085/tema1/Main.java
+++ src/ro/ase/cts/gr1085/tema1/Main.java
@@ -10,9 +10,9 @@ public class Main {
     public static void main(String[] args) {
         MasinaFactory factory = new MasinaFactory();
 
-        InterfataMasina galbena = factory.getMasina("galben"); galbena.setNumarulRoti(3);
-        InterfataMasina rosie = factory.getMasina("portocaliu"); rosie.setNumarulRoti(4);
-        InterfataMasina galbena2 = factory.getMasina("galben"); galbena2.setNumarulRoti(8);
+        InterfataMasina galbena = factory.getMasina("galben"); galbena.setNumarRoti(3);
+        InterfataMasina rosie = factory.getMasina("portocaliu"); rosie.setNumarRoti(4);
+        InterfataMasina galbena2 = factory.getMasina("galben"); galbena2.setNumarRoti(8);
 
         galbena.afiseaza(); // also shows 30 because we modified the original object
         rosie.afiseaza();
