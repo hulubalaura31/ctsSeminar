3129a81017c014c7b61a6f8b0b19fdaf062cc706
Laura <hulubalaura31@gmail.com>
2022-04-28 16:57:33 +0300

    lab09

--- src/ro/ase/cts/gr1085/lab09/Angajat.java
+++ src/ro/ase/cts/gr1085/lab09/Angajat.java
@@ -6,12 +6,10 @@ import java.util.List;
 public class Angajat implements IAngajat{
     private String nume;
     private String codAngajat;
-    private List<IAngajat> angajati = new ArrayList<IAngajat>();
 
     public Angajat(String nume, String codAngajat){
         this.nume = nume;
         this.codAngajat = codAngajat;
-        this.angajati = null;
     }
 
     @Override
--- src/ro/ase/cts/gr1085/lab09/IAngajat.java
+++ src/ro/ase/cts/gr1085/lab09/IAngajat.java
@@ -2,6 +2,6 @@ package ro.ase.cts.gr1085.lab09;
 
 public interface IAngajat {
     public void afisareDetalii();
-    void adaugareSubordonat(IAngajat iAngajat);
-    void stergereSubordonat(IAngajat iAngajat);
+   public void adaugareSubordonat(IAngajat iAngajat);
+    public void stergereSubordonat(IAngajat iAngajat);
 }
